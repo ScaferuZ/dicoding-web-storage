25bef7b7bdab746925572bee66811c5a8397c3d3
Endra Lazuardi <endrafruz@gmail.com>
2021-07-10 22:06:44 +0800

    adding field validation

--- script/script.js
+++ script/script.js
@@ -3,13 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
   const unfinishedButton = document.getElementById("unfinishedBook");
   unfinishedButton.addEventListener("click", function (event) {
     event.preventDefault();
-    addBookToUnfinished();
+    if (document.getElementById("bookName").value.length == 0) {
+      alert("Please fill the field");
+    } else {
+      addBookToUnfinished();
+    }
   });
 
   // put the book added to the finished book display
   const finishedButton = document.getElementById("finishedBook");
   finishedButton.addEventListener("click", function (event) {
     event.preventDefault();
-    addBookToFinished();
+    if (document.getElementById("bookName").value.length == 0) {
+      alert("Please fill the field");
+    } else {
+      addBookToFinished();
+    }
   });
 });
